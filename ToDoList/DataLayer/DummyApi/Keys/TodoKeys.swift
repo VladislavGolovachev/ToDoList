61feb2ea985cb2ef4af68fd22d368218885f0dc2
VladislavGolovachev <serpentess14@gmail.com>
2024-09-09 05:23:06 +0500

    DataLayer finished, but it needs to be checked for stable and correct work

---
+++ ToDoList/DataLayer/DummyApi/Keys/TodoKeys.swift
@@ -0,0 +1,15 @@
+//
+//  TodoKeys.swift
+//  ToDoList
+//
+//  Created by Владислав Головачев on 09.09.2024.
+//
+
+import Foundation
+
+enum TodoKeys: String {
+    case reminder = "reminder"
+    case notes = "notes"
+    case isCompleted = "isCompleted"
+    case date = "date"
+}
